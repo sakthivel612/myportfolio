bde610c9d01c963b1e53ae853d72c92e7d6f7bb0
sakthivel612 <sv69811@gmail.com>
2023-04-29 13:59:32 -0700

    All commits

--- src/components/TopContent/TopContent.js
+++ src/components/TopContent/TopContent.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Link } from "react-scroll";
 import "./TopContent.css";
 
+
 const TopContent = () => {
   return (
     <div className="topContent">
       <div className="topContent_container">
         <h1>Mr.Sakthivel</h1>
         <p>Full Stack Developer</p>
-        <a>
-          <button className="topContent_download_btn">Download CV</button>
+        <a href="https://main--zingy-froyo-221a22.netlify.app/">
+          <button className="topContent_download_btn">
+            My Resume
+          </button>
         </a>
         <Link to="projects" smooth={true} duration={500}>
           <button className="topContent_mywork_btn">My Work</button>
